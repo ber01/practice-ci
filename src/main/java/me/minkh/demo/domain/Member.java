51a0d4d304b85deab1c9f81a3160315dad3ad48a
ber01 <ber01@naver.com>
2024-01-05 08:18:40 +0900

    chore: actions 성공 테스트 커밋

---
+++ src/main/java/me/minkh/demo/domain/Member.java
@@ -0,0 +1,13 @@
+package me.minkh.demo.domain;
+
+public class Member {
+
+    private final String name;
+
+    private final int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/test/java/me/minkh/demo/domain/MemberTest.java
@@ -0,0 +1,15 @@
+package me.minkh.demo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberTest {
+
+    @Test
+    void test() {
+        Member member = new Member("홍길동", 20);
+        assertThat(member).isNotNull();
+    }
+
+}
